12befe7bf49a7110c5286e9371aab7bf23692a0a
Alexander Monk <amon838@UoA.auckland.ac.nz>
2019-08-28 12:19:59 +1200

    working exercise 4

---
+++ software/ass5/obj/default/hello_world.d
@@ -0,0 +1,30 @@
+obj/default/hello_world.o: hello_world.c \
+ ../ass5_bsp//HAL/inc/sys/alt_alarm.h \
+ ../ass5_bsp//HAL/inc/sys/alt_llist.h ../ass5_bsp//HAL/inc/alt_types.h \
+ ../ass5_bsp//HAL/inc/priv/alt_alarm.h \
+ ../ass5_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../ass5_bsp//HAL/inc/io.h ../ass5_bsp//HAL/inc/alt_types.h \
+ ../ass5_bsp/system.h ../ass5_bsp/linker.h \
+ ../ass5_bsp//HAL/inc/sys/alt_irq.h ../ass5_bsp//HAL/inc/nios2.h
+
+../ass5_bsp//HAL/inc/sys/alt_alarm.h:
+
+../ass5_bsp//HAL/inc/sys/alt_llist.h:
+
+../ass5_bsp//HAL/inc/alt_types.h:
+
+../ass5_bsp//HAL/inc/priv/alt_alarm.h:
+
+../ass5_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../ass5_bsp//HAL/inc/io.h:
+
+../ass5_bsp//HAL/inc/alt_types.h:
+
+../ass5_bsp/system.h:
+
+../ass5_bsp/linker.h:
+
+../ass5_bsp//HAL/inc/sys/alt_irq.h:
+
+../ass5_bsp//HAL/inc/nios2.h:
